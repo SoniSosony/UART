a22cda2a6753cfe6cd54715a0fbf56b6e9da5d2d
SoniSosony <antonklimowicz@gmail.com>
2020-04-29 12:31:34 +0200

    Button listeners added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,81 @@
 package sample;
 
+import sample.communication.COMConnect;
+
+import javax.comm.CommPortIdentifier;
+import javax.comm.SerialPort;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+import java.util.Enumeration;
+import java.util.Set;
+
 public class Controller {
+
+    private Enumeration portList;
+    private Thread readThread;
+    private boolean isConnect = false;
+    private SerialPort serialPort;
+
+    @FXML
+    private Button connectButton;
+
+    @FXML
+    private Button scanButton;
+
+    @FXML
+    private ComboBox<String> comboBoxPorts;
+
+    private Object COMConnect;
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if (e.getSource().equals(scanButton)) {
+            portList = CommPortIdentifier.getPortIdentifiers();
+            comboBoxPorts.getItems().clear();
+
+            CommPortIdentifier portId;
+            while (portList.hasMoreElements()) {
+                portId = (CommPortIdentifier) portList.nextElement();
+                if (portId.getName().charAt(0) == 'C') {
+                    System.out.println(portId.getName());
+                    comboBoxPorts.getItems().add(portId.getName());
+                }
+            }
+        }
+        if (e.getSource().equals(connectButton)) {
+            if (isConnect) {
+                serialPort.close();
+                isConnect = false;
+                connectButton.setText("Connect");
+            } else {
+//                if (readThread != null) {
+//                    Set<Thread> setOfThread = Thread.getAllStackTraces().keySet();
+//                    for (Thread thread : setOfThread) {
+//                        if (thread.getId() == readThread.getId())
+//                            System.out.println("Thread found");
+//                        thread.interrupt();
+//                    }
+//                }
+
+                System.out.println("Connecting...");
+                portList = CommPortIdentifier.getPortIdentifiers();
+                CommPortIdentifier portId;
+                while (portList.hasMoreElements()) {
+                    portId = (CommPortIdentifier) portList.nextElement();
+                    if (portId.getName().equals(comboBoxPorts.getValue())) {
+                        COMConnect reader = new COMConnect(portId);
+                        serialPort = reader.getSerialPort();
+                        readThread = reader.getReadThread();
+                        System.out.println("New reader created");
+                        isConnect =true;
+                        connectButton.setText("Disconnect");
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ src/sample/communication/COMConnect.java
@@ -0,0 +1,89 @@
+package sample.communication;
+
+import javax.comm.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Set;
+import java.util.TooManyListenersException;
+
+public class COMConnect implements Runnable, SerialPortEventListener {
+
+    static Enumeration portList;
+
+    InputStream inputStream;
+
+    SerialPort serialPort;
+
+    Thread readThread;
+
+    public Thread getReadThread() {
+        return readThread;
+    }
+
+    public SerialPort getSerialPort() {
+        return serialPort;
+    }
+
+    public void setSerialPort(SerialPort serialPort) {
+        this.serialPort = serialPort;
+    }
+
+    public COMConnect(CommPortIdentifier portId) {
+        try {
+            serialPort = (SerialPort) portId.open("COMConnectApp", 2000);
+        } catch (PortInUseException e) {
+        }
+        try {
+            inputStream = serialPort.getInputStream();
+        } catch (IOException e) {
+        }
+        try {
+            serialPort.addEventListener(this);
+        } catch (TooManyListenersException e) {
+        }
+        serialPort.notifyOnDataAvailable(true);
+        try {
+            serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+        } catch (UnsupportedCommOperationException e) {
+        }
+        readThread = new Thread(this);
+        readThread.start();
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent event) {
+        switch (event.getEventType()) {
+            case SerialPortEvent.BI:
+            case SerialPortEvent.OE:
+            case SerialPortEvent.FE:
+            case SerialPortEvent.PE:
+            case SerialPortEvent.CD:
+            case SerialPortEvent.CTS:
+            case SerialPortEvent.DSR:
+            case SerialPortEvent.RI:
+            case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
+                break;
+            case SerialPortEvent.DATA_AVAILABLE:
+                byte[] readBuffer = new byte[20];
+
+                try {
+                    while (inputStream.available() > 0) {
+                        int numBytes = inputStream.read(readBuffer);
+
+                    }
+                    System.out.print(new String(readBuffer));
+                } catch (IOException e) {
+                }
+                break;
+        }
+    }
+}
